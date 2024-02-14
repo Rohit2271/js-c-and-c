a3ccb67691a8991228b28937319c0d223bcbe5f4
Rohit2271 <148337396+Rohit2271@users.noreply.github.com>
2024-02-14 05:35:30 +0000

    data types

---
+++ pracitce/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+let name = "Rohit"
+let age = 30
+let isLoggedIn = false
+
+
+// number =. 2 to power 53
+// bigint
+//string => ""
+// boolean =>true/false
+// null => standalone value
+// undefined => value not asssignedd yet
+// symbol => unique
+
+//object
+
+console.log(typeof "Rohit") //string
+console.log(typeof null); ///object
+console.log(typeof undefined) //undefined
