2106f2d44258fa0ca4eee73045afb68299adc855
Rohit2271 <148337396+Rohit2271@users.noreply.github.com>
2024-02-14 04:08:42 +0000

    variable practice

---
+++ pracitce/variables.js
@@ -0,0 +1,13 @@
+const accountId = 155423
+let accountEmail = "yadav10oct92@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaunpur"
+
+//let accountEmail = "abcd@gmail.com"// not allowed for let already declaired
+//accountId = 2 //not allowed for const
+//var accountPassword = "54321"// change password so this reason to not use var 
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
