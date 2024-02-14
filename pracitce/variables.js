87dc8b567426d8337d8109f0548b57e2887de95c
Rohit2271 <148337396+Rohit2271@users.noreply.github.com>
2024-02-14 04:29:20 +0000

    variables tables in terminal

--- pracitce/variables.js
+++ pracitce/variables.js
@@ -2,12 +2,21 @@ const accountId = 155423
 let accountEmail = "yadav10oct92@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaunpur"
+let accountState;
 
-//let accountEmail = "abcd@gmail.com"// not allowed for let already declaired
+//accountEmail = "abcd@gmail.com"// allowed variable
 //accountId = 2 //not allowed for const
 //var accountPassword = "54321"// change password so this reason to not use var 
 
+/*
 console.log(accountId)
 console.log(accountEmail)
 console.log(accountPassword)
 console.log(accountCity)
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+prefer not to use var beacause of issue in block scope and functional scope
+*/
